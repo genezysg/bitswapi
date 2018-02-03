ecb67d5e014711b0828c0ff57124ec5e58670b30
Genezys <genezys@gmail.com>
2018-02-03 11:39:59 -0200

    Adds more properties to server

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 var restify = require('restify');
 var config = require('config');
 
-const server = restify.createServer();
+const server = restify.createServer({
+    name:'dsfbit',
+    version:'1.0.0'
+});
+
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
 
 
 function respond(req, res, next) {
   res.send('hello ' + req.params.name);
-  next();
+  next()
 }
 
 server.get('/hello/:name', respond);
