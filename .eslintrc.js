82da713fa6ea103ef6c993c27ead9cc36127a01b
Genezys <genezys@gmail.com>
2018-02-06 02:53:02 -0200

    Remove rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -195,7 +195,7 @@ module.exports = {
         "padding-line-between-statements": "error",
         "prefer-arrow-callback": "off",
         "prefer-const": "error",
-        "prefer-destructuring": "error",
+        "prefer-destructuring": "off",
         "prefer-numeric-literals": "error",
         "prefer-promise-reject-errors": "error",
         "prefer-reflect": "error",
