dcb3b138236f99a2071707a50b95bc41df5687c7
Genezys <genezys@gmail.com>
2018-02-05 22:43:51 -0200

    Fixes some lint errors

--- clients/swapi.js
+++ clients/swapi.js
@@ -1,20 +1,30 @@
 const request = require('request')
-const qs = require('querystring')
-
-const url="https://swapi.co"
-const planetPath ="api/planets/"
 
 
-exports.getAppearances=(planet,callback)=>{
-	let param={search:planet}
-	request.get({url:"https://swapi.co/api/planets",qs:param,headers:{"Content-Type":"application/json"}},(err,res) => {
-		let json=JSON.parse(res.body)
-		const found=json.results.find((element)=>{
-			if (element.name==planet) return element
-		})
-		if (found===undefined){
-			callback(err,0)
-		}
-		callback(err,found.films.length)
-	})
+exports.getAppearances=(planet,callback) => {
+    const param={search:planet}
+
+    request.get(
+        {
+        url:'https://swapi.co/api/planets',
+        qs:param,
+        headers:{'Content-Type':'application/json'}
+        },
+        (err,res) => {
+        const json=JSON.parse(res.body)
+        let found=null;
+
+        found=json.results.find((element) => {
+            if (element.name===planet) {
+                return element;
+            }
+        })
+        if (found===null) {
+            callback(err,0)
+
+            return;
+        }
+        callback(err,found.films.length)
+    }
+    )
 }
