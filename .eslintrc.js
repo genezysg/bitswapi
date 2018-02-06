b8ad8f13ed88b869f4d3c4cda8b22ebce0d30337
Genezys <genezys@gmail.com>
2018-02-05 22:40:11 -0200

    Remove some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,7 +36,7 @@ module.exports = {
         "consistent-this": "error",
         "curly": "error",
         "default-case": "error",
-        "dot-location": "error",
+        "dot-location": "off",
         "dot-notation": "error",
         "eol-last": "error",
         "eqeqeq": "error",
@@ -217,10 +217,7 @@ module.exports = {
             "last"
         ],
         "sort-imports": "error",
-        "sort-keys": [
-            "error",
-            "asc"
-        ],
+        "sort-keys": 'off',
         "sort-vars": "error",
         "space-before-blocks": "error",
         "space-before-function-paren": "off",
