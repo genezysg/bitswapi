d1662de0a24e622916054f814dbe8b0a21af3cac
Genezys <genezys@gmail.com>
2018-02-07 01:20:02 -0200

    Adds 'get appearances in movies' on planet model

--- models/planet.js
+++ models/planet.js
@@ -1,13 +1,31 @@
 const mongoose=require('mongoose');
+const swapi=require('../clients/swapi')
 
 var planetSchema = new mongoose.Schema({
   climate: String,
-  movieAppearances: Number,
   name:  String,
   terrain: String,
-  updateAt: Date
+  updateAt: Date,
+  movieAppearances: {
+                     type:Number,
+                     default:0
+                    }
 });
 
 
+
+planetSchema.methods.getAppearances=function () {
+    var that=this
+
+    swapi.getAppearances(that.name,(err,appearances) => {
+        if (err) {
+            return;
+        }
+        if (that.movieAppearances !== appearances) {
+            that.movieAppearances=appearances
+            that.save()
+        }
+    })
+}
 
 module.exports = mongoose.model('Planet',planetSchema)
