c7e5fd120975bd481a1fdc2b5a1fac98306b9fea
Genezys <genezys@gmail.com>
2018-02-07 03:45:15 -0200

    Colorize logger msgs

--- logger.js
+++ logger.js
@@ -3,10 +3,17 @@ const config=require('config')
 const File=winston.transports.File
 const Console=winston.transports.Console
 
+
+
+
 const transportFile=new File({
                             filename:'server.log',
-                            level:config.get('log_level.file')
+                            level:config.get('log_level.file'),
+                            colorize: true
                             })
-const trconsole = new Console({level:config.get('log_level.console')});
+const trconsole = new Console({
+                                level:config.get('log_level.console'),
+                                colorize:true
+                            });
 
 module.exports=new winston.Logger({transports:[trconsole,transportFile]});
