73adc27b25afa8767b2ac3a601681f8a72478224
Genezys <genezys@gmail.com>
2018-02-11 20:00:15 -0200

    Change way to load url to mongoose

--- mongoose/connect.js
+++ mongoose/connect.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 var config=require('config')
 var logger = require('../logger')
 
-mongoose.connect(`mongodb://localhost/${config.db}`).catch((error) => {
+mongoose.connect(config.mongooseURL).catch((error) => {
         logger.error('Mongoose não está connectado',error)
     });
 
